cce506040b136a9b36b5a6abd0f265f0a63dd1bd
nbd1994 <natnaeljul7@gmail.com>
2024-05-15 15:57:30 +0300

    CoreFileAdded

--- app/controllers/_404.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class _404 extends Controller{
-    function index()
-    {
-        $data['title'] = "404";
-        $this->view('404', $data);
-    }
-}
--- app/controllers/index.php
+++
@@ -1,2 +0,0 @@
-<?php
-echo "Can't access this page..";
